295a254da900d92e61fd10cf659fead884cb27c4
VaibhavVinayakChaudhari <134763341+VaibhavVinayakChaudhari@users.noreply.github.com>
2023-10-07 17:42:25 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo `env`
+cd /local/git/docker-mongo
+git pull
+
+# start mongo db
+mongod --fork --logpath /tmp/mongodb.log --rest --httpinterface --smallfiles
+mongod --rest --httpinterface --smallfiles
+
+# wait for mongo to load
+#sleep 30
+
+# load demo data
+#cd /local/git/docker-mongo/northwind-mongo-master/
+#./mongo-import.sh
